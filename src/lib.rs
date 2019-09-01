f872820cd4432979d04ff3a3cefc02206360ccef
steve olsen <dev@steve.computer>
2019-09-01 12:49:13 -0400

    some dpr resizing stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,23 @@
+use std::f64;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::console;
 
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-// This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
-
-    // Your code goes here!
     console::log_1(&JsValue::from_str("This is Metvan!"));
-
     Ok(())
 }
 
 #[wasm_bindgen]
 pub struct Game {
+    dpr: f64,
     canvas: web_sys::HtmlCanvasElement,
     ctx: web_sys::CanvasRenderingContext2d,
 }
@@ -49,7 +41,10 @@ impl Game {
             .dyn_into::<web_sys::CanvasRenderingContext2d>()
             .unwrap();
 
-        Ok(Self { canvas, ctx })
+        let dpr = web_sys::window().unwrap().device_pixel_ratio();
+        console::log_1(&JsValue::from_str(&format!("dpr: {}", dpr)));
+
+        Ok(Self { dpr, canvas, ctx })
     }
 
     pub fn update(&mut self, _t: f64) -> Result<(), JsValue> {
@@ -57,8 +52,9 @@ impl Game {
         let display_height = self.canvas.client_height() as u32;
 
         if self.canvas.width() != display_width || self.canvas.height() != display_height {
-            self.canvas.set_width(display_width);
-            self.canvas.set_height(display_height);
+            self.canvas.set_width(display_width * self.dpr as u32);
+            self.canvas.set_height(display_height * self.dpr as u32);
+            self.ctx.scale(self.dpr, self.dpr)?;
             console::log_1(&JsValue::from_str("Resizing from rust"));
         }
 
@@ -69,6 +65,20 @@ impl Game {
         self.ctx.fill_rect(width - 20.0, 10.0, 10.0, 10.0);
         self.ctx.fill_rect(width - 20.0, height - 20.0, 10.0, 10.0);
         self.ctx.fill_rect(10.0, height - 20.0, 10.0, 10.0);
+
+        self.ctx.save();
+
+        // @Q: Why can this fail?
+        self.ctx.translate(width / 2.0, height / 2.0)?;
+        // Now we have 0,0 in the center of the screen
+        self.ctx.set_fill_style(&JsValue::from_str("red"));
+        self.ctx.fill_rect(-5.0, -20.0, 10.0, 20.0);
+        self.ctx.set_fill_style(&JsValue::from_str("black"));
+        self.ctx.begin_path();
+        self.ctx.arc(0.0, 0.0, 5.0, 0.0, f64::consts::PI * 2.0)?;
+        self.ctx.stroke();
+
+        self.ctx.restore();
 
         Ok(())
     }
