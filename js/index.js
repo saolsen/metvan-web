c8ed727d5bb0cd1cb7d76168348df48dc91a9ad1
steve olsen <dev@steve.computer>
2019-09-02 13:29:18 -0400

    player input

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 import("../pkg/index.js")
     .then(module => {
-        let game = module.Game.new();
+        let platform = module.Platform.new();
+
+        function onkey(ev, key, pressed) {
+            platform.onkey(key, pressed);
+            ev.preventDefault();
+        }
+
+        document.addEventListener('keydown', function (ev) { return onkey(ev, ev.keyCode, true); }, false);
+        document.addEventListener('keyup', function (ev) { return onkey(ev, ev.keyCode, false); }, false);
 
         function renderLoop(t) {
-            game.update(t);
+            platform.update(t);
             requestAnimationFrame(renderLoop);
         }
         renderLoop(0);
--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,39 @@ macro_rules! console_log {
     ($($t:tt)*) => (console::log_1(&JsValue::from_str(&format_args!($($t)*).to_string())))
 }
 
+#[derive(Debug, Copy, Clone)]
+pub struct V2 {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl V2 {
+    pub fn zero() -> Self {
+        V2 { x: 0.0, y: 0.0 }
+    }
+}
+
+#[derive(Debug)]
+pub struct Input {
+    pub left: bool,
+    pub right: bool,
+}
+
+#[derive(Debug)]
+pub struct Game {
+    p: V2,
+    dp: V2,
+}
+
+impl Game {
+    pub fn new() -> Self {
+        Self {
+            p: V2::zero(),
+            dp: V2::zero(),
+        }
+    }
+}
+
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
@@ -18,16 +51,134 @@ pub fn main_js() -> Result<(), JsValue> {
     Ok(())
 }
 
-#[wasm_bindgen]
-pub struct Game {
-    dpr: f64,
-    canvas: web_sys::HtmlCanvasElement,
-    ctx: web_sys::CanvasRenderingContext2d,
+#[derive(Debug)]
+pub enum Key {
+    Unknown,
+    Backspace,
+    Tab,
+    Return,
+    Esc,
+    Space,
+    PageUp,
+    PageDown,
+    End,
+    Home,
+    Left,
+    Right,
+    Down,
+    Insert,
+    Delete,
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    L,
+    M,
+    N,
+    O,
+    P,
+    Q,
+    R,
+    S,
+    T,
+    U,
+    V,
+    W,
+    X,
+    Y,
+    Z,
+    Tilda,
+}
+
+impl From<u32> for Key {
+    fn from(v: u32) -> Key {
+        match v {
+            8 => Key::Backspace,
+            9 => Key::Tab,
+            13 => Key::Return,
+            27 => Key::Esc,
+            32 => Key::Space,
+            33 => Key::PageUp,
+            34 => Key::PageDown,
+            35 => Key::End,
+            36 => Key::Home,
+            37 => Key::Left,
+            39 => Key::Right,
+            40 => Key::Down,
+            45 => Key::Insert,
+            46 => Key::Delete,
+            48 => Key::Zero,
+            49 => Key::One,
+            50 => Key::Two,
+            51 => Key::Three,
+            52 => Key::Four,
+            53 => Key::Five,
+            54 => Key::Six,
+            55 => Key::Seven,
+            56 => Key::Eight,
+            57 => Key::Nine,
+            65 => Key::A,
+            66 => Key::B,
+            67 => Key::C,
+            68 => Key::D,
+            69 => Key::E,
+            70 => Key::F,
+            71 => Key::G,
+            72 => Key::H,
+            73 => Key::I,
+            74 => Key::J,
+            75 => Key::K,
+            76 => Key::L,
+            77 => Key::M,
+            78 => Key::N,
+            79 => Key::O,
+            80 => Key::P,
+            81 => Key::Q,
+            82 => Key::R,
+            83 => Key::S,
+            84 => Key::T,
+            85 => Key::U,
+            86 => Key::V,
+            87 => Key::W,
+            88 => Key::X,
+            89 => Key::Y,
+            90 => Key::Z,
+            192 => Key::Tilda,
+            _ => Key::Unknown,
+        }
+    }
 }
 
 #[wasm_bindgen]
-impl Game {
-    pub fn new() -> Result<Game, JsValue> {
+pub struct Platform {
+    dpr: f64,
+    canvas: web_sys::HtmlCanvasElement,
+    ctx: web_sys::CanvasRenderingContext2d,
+    // game stuff
+    input: Input,
+    game: Game,
+}
+
+#[wasm_bindgen]
+impl Platform {
+    pub fn new() -> Result<Platform, JsValue> {
         console_log!("Setting up Game");
 
         let document = web_sys::window().unwrap().document().unwrap();
@@ -47,10 +198,45 @@ impl Game {
         let dpr = web_sys::window().unwrap().device_pixel_ratio();
         console::log_1(&JsValue::from_str(&format!("Dpr: {}", dpr)));
 
-        Ok(Self { dpr, canvas, ctx })
+        let game = Game::new();
+        console_log!("{:?}", game);
+
+        let input = Input {
+            left: false,
+            right: false,
+        };
+
+        Ok(Self {
+            dpr,
+            canvas,
+            ctx,
+            input,
+            game,
+        })
+    }
+
+    pub fn onkey(&mut self, key: u32, pressed: bool) {
+        let key: Key = key.into();
+        match key {
+            Key::A => self.input.left = pressed,
+            Key::D => self.input.right = pressed,
+            _ => (),
+        };
+        console_log!("{:?}: pressed?: {}", key, pressed);
     }
 
     pub fn update(&mut self, _t: f64) -> Result<(), JsValue> {
+        //self.game.p.x = 0.0;
+        //self.game.p.y = 0.0;
+
+        if self.input.left {
+            self.game.p.x -= 1.0;
+        }
+
+        if self.input.right {
+            self.game.p.x += 1.0;
+        }
+
         let display_width = self.canvas.client_width() as u32 * self.dpr as u32;
         let display_height = self.canvas.client_height() as u32 * self.dpr as u32;
 
@@ -64,12 +250,16 @@ impl Game {
         let width = self.canvas.client_width() as f64;
         let height = self.canvas.client_height() as f64;
 
+        self.ctx.clear_rect(0.0, 0.0, width, height);
+
         self.ctx.fill_rect(10.0, 10.0, 10.0, 10.0);
         self.ctx.fill_rect(width - 20.0, 10.0, 10.0, 10.0);
         self.ctx.fill_rect(width - 20.0, height - 20.0, 10.0, 10.0);
         self.ctx.fill_rect(10.0, height - 20.0, 10.0, 10.0);
 
         self.ctx.save();
+        self.ctx
+            .translate(self.game.p.x as f64, -self.game.p.y as f64)?;
 
         // @Q: Why can this fail?
         self.ctx.translate(width / 2.0, height / 2.0)?;
