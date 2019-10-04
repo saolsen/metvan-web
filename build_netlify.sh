e1457cbfb7d4101e4ab4851a34247d76fe530eb9
steve olsen <dev@steve.computer>
2019-10-04 14:17:02 -0400

    no rust

--- build_netlify.sh
+++ build_netlify.sh
@@ -2,9 +2,9 @@ wget http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.
 tar -xvzf clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 ls
 which clang
-clang --version
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-source $HOME/.cargo/env
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
-npm install
-npm run build
+#clang --version
+#curl https://sh.rustup.rs -sSf | sh -s -- -y
+#source $HOME/.cargo/env
+#curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+#npm install
+#npm run build
