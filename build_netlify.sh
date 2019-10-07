dca7e5af55ad7f265d870d720694e485b0fba118
steve olsen <dev@steve.computer>
2019-10-07 13:31:50 -0400

    almost got it

--- build_netlify.sh
+++ build_netlify.sh
@@ -1,6 +1,6 @@
 wget --quiet http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 tar -xf clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-export PATH="clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
+export PATH="clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/bin:${PATH}"
 export LDFLAGS="-Lclang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/lib"
 export CPPFLAGS="-Lclang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/include"
 . build_web.sh
