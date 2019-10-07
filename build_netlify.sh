f1950ed51fa869adab2e7d783894df6ee42987ce
steve olsen <dev@steve.computer>
2019-10-07 13:41:19 -0400

    logs

--- build_netlify.sh
+++ build_netlify.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+echo "Downloading Clang"
 wget --quiet http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 tar -xf clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 export PATH="`pwd`/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/bin:${PATH}"
-echo ${PATH}
-echo $PATH
 #export LDFLAGS="-Lclang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/lib"
 #export CPPFLAGS="-Lclang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/include"
 ./build_web.sh
--- build_web.sh
+++ build_web.sh
@@ -5,9 +5,11 @@
 
 CFLAGS="-Wall -Os -g -fdiagnostics-absolute-paths"
 
+echo "Compiling"
 mkdir -p dist
 pushd dist
 clang $CFLAGS --target=wasm32 -nostdlib -Wl,--no-entry -Wl,--export-all -Wl,--allow-undefined -Wl,--import-memory -o metvan.wasm ../src/web_platform.c
 cp ../src/web_platform_index.html index.html
 cp ../src/web_platform.js web_platform.js
 popd
+echo "Done"
