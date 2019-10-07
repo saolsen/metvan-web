053ba2b49bebb245a40daec264cc5ff05fbddf31
steve olsen <dev@steve.computer>
2019-10-07 13:34:03 -0400

    make it bash?

--- build_netlify.sh
+++ build_netlify.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 wget --quiet http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 tar -xf clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 export PATH="clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/bin:${PATH}"
