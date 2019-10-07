45d40c673ecc117c0c3661ad610de96b698042fe
steve olsen <dev@steve.computer>
2019-10-07 13:20:35 -0400

    try just calling build script

--- build_netlify.sh
+++ build_netlify.sh
@@ -3,4 +3,4 @@ tar -xf clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 export PATH="clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/bin:PATH"
 export LDFLAGS="-Lclang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/lib"
 export CPPFLAGS="-Lclang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/include"
-. build_web.sh
+./build_web.sh
