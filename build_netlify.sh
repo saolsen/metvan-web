2f3b543676f70bdc2484301ceb62afec21150fdc
steve olsen <dev@steve.computer>
2019-10-07 13:35:23 -0400

    normal var?

--- build_netlify.sh
+++ build_netlify.sh
@@ -2,7 +2,7 @@
 
 wget --quiet http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 tar -xf clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-export PATH="clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/bin:${PATH}"
-export LDFLAGS="-Lclang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/lib"
-export CPPFLAGS="-Lclang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/include"
+PATH="clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/bin:${PATH}"
+#export LDFLAGS="-Lclang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/lib"
+#export CPPFLAGS="-Lclang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/include"
 . build_web.sh
